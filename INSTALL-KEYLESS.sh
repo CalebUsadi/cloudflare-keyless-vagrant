5994b4b6622d6d7b6301df9ad652577d53ddfc3d
Ben <ben@bengreenier.com>
2015-04-01 12:23:14 -0400

    Update INSTALL-KEYLESS.sh

--- INSTALL-KEYLESS.sh
+++ INSTALL-KEYLESS.sh
@@ -4,6 +4,7 @@ sudo apt-get install -y wireshark tshark
 sudo chmod 4711 `which dumpcap` #for wireshark to function properly
 sudo apt-get install -y git
 sudo apt-get install -y make gcc unzip automake libtool
+sudo apt-get install -y dos2unix
 echo Got dependencies and tools
 if [ -d "keyless" ]; then
   echo Keyless directory already here, updating git repository
@@ -19,4 +20,8 @@ echo Tried to make and install keyless
 cd ..
 cp /vagrant/CAPTURE-TEST.sh CAPTURE-TEST.sh
 cp /vagrant/COPY-TEST-RESULTS.sh COPY-TEST-RESULTS.sh
+dos2unix CAPTURE-TEST.sh
+dos2unix COPY-TEST-RESULTS.sh
+chmod +x CAPTURE-TEST.sh
+chmod +x COPY-TEST-RESULTS.sh
 echo Placed CAPTURE-TEST.sh and COPY-TEST-RESULTS.sh in home directory
